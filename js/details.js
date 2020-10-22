76635adba0a2fcf50dc440678cf94b32681cbd06
Tor Magne <61709493+TorMagne@users.noreply.github.com>
2020-10-22 19:32:20 +0200

    Initial commit

---
+++ js/details.js
@@ -0,0 +1,32 @@
+const cardDetails = document.querySelector('.container');
+const queryString = document.location.search;
+const params = new URLSearchParams(queryString);
+const id = params.get('id');
+const url = 'https://api.pokemontcg.io/v1/cards?' + id;
+
+console.log(id);
+
+async function tcgCard() {
+  try {
+    const response = await fetch(url);
+    const json = await response.json();
+    pokemon = json.cards;
+    console.log(pokemon);
+
+    cardDetails.innerHTML = `<div class="card">
+          <img class="pokemon-card" src="${pokemon.imageUrlHiRes}" alt="pokemon image">
+            <div class="card-header">
+              <h4 class="card-name">${pokemon.name}</h4>
+              <p class="card-para">Artist: ${pokemon.artist}</hp>
+              <p class="card-para">Set: ${pokemon.set}</p>
+              <p class="card-para">Series: ${pokemon.series}</p>
+              <p class="card-para">PokeDex Number: ${pokemon.nationalPokedexNumber}</p>
+              <p class="card-para">Weaknesses: ${pokemon.weaknesses.type}</p>
+            </div>
+            </div>`;
+  } catch (error) {
+    // console.log(error);
+  }
+}
+
+tcgCard();
---
+++ js/index.js
@@ -0,0 +1,30 @@
+const container = document.querySelector('.container');
+const url = 'https://api.pokemontcg.io/v1/cards';
+
+async function tcg() {
+  try {
+    const response = await fetch(url);
+    const json = await response.json();
+    pokemon = json.cards;
+    console.log(pokemon);
+
+    for (let i = 0; i < pokemon.length; i++) {
+      if (i === 50) {
+        break;
+      }
+
+      container.innerHTML += `<a class="card" href="details.html?id=${pokemon[i].id}">
+        <img class="pokemon-card" src="${pokemon[i].imageUrl}" alt="pokemon image">
+          <div class="card-header">
+            <h4 class="card-name">${pokemon[i].name}</h4>
+            <p class="card-para">Set: ${pokemon[i].set}</p>
+          </div>
+          </a>
+      `;
+    }
+  } catch (error) {
+    // console.log(error);
+  }
+}
+
+tcg();
