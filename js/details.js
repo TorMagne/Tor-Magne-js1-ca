5df3502efe71b9815539d56d2bd55ece27f5db72
Tor Magne <61709493+TorMagne@users.noreply.github.com>
2020-10-23 21:14:49 +0200

    Trying to fix API

--- js/details.js
+++ js/details.js
@@ -1,22 +1,22 @@
 const cardDetails = document.querySelector('.container');
 const queryString = document.location.search;
-const params = new URLSearchParams(queryString);
+const params = new URLSearchParams('queryString');
 const id = params.get('id');
-const url = 'https://api.pokemontcg.io/v1/cards?' + id;
+const url = 'https://api.pokemontcg.io/v1/cards/xy7-4';
 
-console.log(id);
+// console.log(params);
 
 async function tcgCard() {
   try {
     const response = await fetch(url);
     const json = await response.json();
-    pokemon = json.cards;
-    console.log(pokemon);
+    // pokemon = json.cards;
+    console.log(json);
 
     cardDetails.innerHTML = `<div class="card">
-          <img class="pokemon-card" src="${pokemon.imageUrlHiRes}" alt="pokemon image">
+          <img class="pokemon-card" src="${json.imageUrlHiRes}" alt="pokemon image">
             <div class="card-header">
-              <h4 class="card-name">${pokemon.name}</h4>
+              <h4 class="card-name">${json.name}</h4>
               <p class="card-para">Artist: ${pokemon.artist}</hp>
               <p class="card-para">Set: ${pokemon.set}</p>
               <p class="card-para">Series: ${pokemon.series}</p>
