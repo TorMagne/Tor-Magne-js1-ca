bdc9e2a44bab5e27063631d7b59ce4dee4c4f17c
Tor Magne <61709493+TorMagne@users.noreply.github.com>
2020-10-30 18:11:40 +0100

    added form validation true javavascript to form

--- js/form.js
+++ js/form.js
@@ -1,14 +1,59 @@
 const form = document.querySelector('#contact-form');
-const Name = document.querySelector('#name');
+const name = document.querySelector('#name');
 const nameError = document.querySelector('#nameError');
+const subject = document.querySelector('#subject');
+const subjectError = document.querySelector('#subjectError');
+const address = document.querySelector('#address');
+const addressError = document.querySelector('#addressError');
+const email = document.querySelector('#email');
+const emailError = document.querySelector('#emailError');
+const succsess = document.querySelector('.succsess');
 
 function formValidation(event) {
   event.preventDefault();
-  //   form.reset();
-  if (name.value.length > 0) {
+
+  if (checkLength(name.value, 0) === true) {
     nameError.style.display = 'none';
+  } else {
+    nameError.style.display = 'block';
   }
-  console.log('hello');
+
+  if (checkLength(subject.value, 9) === true) {
+    subjectError.style.display = 'none';
+  } else {
+    subjectError.style.display = 'block';
+  }
+
+  if (checkLength(address.value, 24) === true) {
+    addressError.style.display = 'none';
+  } else {
+    addressError.style.display = 'block';
+  }
+
+  if (validateEmail(email.value) === true) {
+    emailError.style.display = 'none';
+  } else {
+    emailError.style.display = 'block';
+  }
+
+  if (validateEmail(form.value) === true) {
+    succsess.style.display = 'block';
+  }
+  // form.reset();
 }
 
 form.addEventListener('submit', formValidation);
+
+function checkLength(value, len) {
+  if (value.trim().length > len) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function validateEmail(email) {
+  const regEx = /\S+@\S+\.\S+/;
+  const matchPattern = regEx.test(email);
+  return matchPattern;
+}
