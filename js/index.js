c9d3fdba30c15859edcb388afd11745a43fd87ef
Tor Magne <tormagne-brekken@outlook.com>
2021-06-01 18:24:49 +0200

    added search function for card to the site

--- js/index.js
+++ js/index.js
@@ -28,9 +28,44 @@ async function tcgCards() {
     }
   } catch (error) {
     container.innerHTML = displayError(
-      "An error occured when loading the API for all the tcg cards"
+      "An error occured when loading the API for the tcg cards"
     );
   }
 }
 
 tcgCards();
+
+const searchCards = (event) => {
+  event.preventDefault();
+  container.innerHTML = "";
+  const pokemon = document.querySelector("#search-pokemon").value.trim();
+  // console.log(pokemon);
+
+  const url = "https://api.pokemontcg.io/v1/cards?name=" + pokemon;
+  const getPokemonUrl = async () => {
+    try {
+      const response = await fetch(url);
+      const json = await response.json();
+      pokemonSearch = json.cards;
+      console.log(pokemonSearch);
+      for (let i = 0; i < pokemonSearch.length; i++) {
+        container.innerHTML += `<a class="card" href="details.html?id=${pokemonSearch[i].id}">
+        <img class="pokemon-card" src="${pokemonSearch[i].imageUrl}" alt="pokemon image">
+          <div class="card-header">
+            <h4 class="card-name">${pokemonSearch[i].name}</h4>
+            <p class="card-para">Set: ${pokemonSearch[i].set}</p>
+          </div>
+          </a>
+      `;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  getPokemonUrl();
+};
+
+const submit = document.querySelector("#submit");
+
+submit.addEventListener("click", searchCards);
